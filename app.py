9b72bd4cbd45cebc20c77cb9fce2a4f6b500e76f
Callymags <callanmaguire18@gmail.com>
2021-08-24 12:48:50 +0000

    Log message to browser

--- app.py
+++ app.py
@@ -18,22 +18,21 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 # Configure actual connection string = Mongo_URI
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 # Grab secret key which is a requirement when running flask functions
-app.secret.key = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # Set up an instance of PyMongo and add app to it using constructor method
 mongo = PyMongo(app)
-
 
 
 # Test function with a route in it that will
 # display some text to window as proof app works
 # "/" refers to default root
 @app.route("/")
-@app.route('/get_tasks')
+@app.route("/get_tasks")
 def get_tasks():
     # Find all docs from tasks collection and assign them to tasks var 
     tasks = mongo.db.tasks.find()
-    # 1st tasks = what template will use, 2nd tasks is tasks var above
+    # 1st tasks is what template will use, 2nd tasks is tasks var above
     return render_template("tasks.html", tasks=tasks)
 
 
