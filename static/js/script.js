26bd28b1fb6fb3c6bb1f2aa46f7b55a2df4c0ca0
Callymags <callanmaguire18@gmail.com>
2021-08-26 08:44:12 +0000

    Add task collpsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 // Jquery to initialize mobile navbar toggler
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"}); // edge = Materialize class to push toggler to right
+    $('.collapsible').collapsible(); // Initialize Collapsible Component
   });
