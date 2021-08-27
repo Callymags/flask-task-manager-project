578f63e62f7704ec42aa0cbc8f6ea9d6cec3158b
Callymags <callanmaguire18@gmail.com>
2021-08-27 07:38:35 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
-// Jquery to initialize mobile navbar toggler
+// Jquery to initialize Materialize elements
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"}); // edge = Materialize class to push toggler to right
     $('.collapsible').collapsible(); // Initialize Collapsible Component
     $('.tooltipped').tooltip(); // Initialize Tooltip
+    $('.datepicker').datepicker({
+      format: 'dd mmmm, yyyy',
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: 'Select'
+      }
+    }); // Initialize datepicker
   });
